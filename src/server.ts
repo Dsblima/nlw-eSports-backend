209857687ba19cbe7c11f5215d4d27ba1c794d9a
Dsblima <dsbl@ecomp.poli.br>
2022-09-14 06:45:43 -0300

    Create basic routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,17 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games',(request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads',(request, response) => {
+  return response.json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  //const gameId = request.params.id
+  
   return response.json([
     {id: 1, name:'Anúncio 1'},
     {id: 2, name:'Anúncio 2'},
@@ -10,6 +20,12 @@ app.get('/ads', (request, response) => {
     {id: 3, name:'Anúncio 4'},
     {id: 3, name:'Anúncio 5'},
   ])
-})
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  //const addId = request.params.id
+  
+  return response.json([])
+});
 
 app.listen(3333)
